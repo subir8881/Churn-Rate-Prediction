dd16fa0a74800d414c4ec6c538b1562884f9ab28
subir8881 <72200125+subir8881@users.noreply.github.com>
2022-01-19 18:16:46 +0530

    Add files via upload

---
+++ model.webapp.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pickle
+
+
+log_model = pickle.load(open('log_reg.pkl', 'rb'))
+
+
+def classify(num):
+    if num == 0:
+        return 'NO'
+    else:
+        return 'YES'
+
+
+def main(hl=None):
+    st.title("CURN RATE PREDICTION")
+    html_temp = """
+    """
+
+    st.markdown(html_temp, unsafe_allow_html=True)
+    activities = ['Logistic Regression']
+    option = st.sidebar.selectbox('Which model you would like to use?', activities)
+    st.subheader(option)
+    st.spinner("Hello")
+    hl = st.selectbox('Resort', 'City Hotel')
+    lt = st.slider('Lead Time', )
+    WN = st.slider('stays in weekend nights', )
+    wn = st.slider('stays in week nights', )
+    ad = st.slider('Select Adults', )
+    ch = st.slider('Select Children', )
+    ba = st.slider('Select Babies', )
+    inputs = [[hl, lt, WN, wn, ad, ch, ba]]
+    if st.button('Classify'):
+        if option == 'Logistic Regression':
+            st.success(classify(log_reg.predict(inputs)))
+
+
+if __name__ == '__main__':
+    main()
