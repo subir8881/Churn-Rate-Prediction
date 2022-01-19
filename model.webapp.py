b1ff088f1c21a09d57d96bd7d6227e15a8357d14
subir8881 <72200125+subir8881@users.noreply.github.com>
2022-01-19 18:44:07 +0530

    Update model.webapp.py

--- model.webapp.py
+++ model.webapp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pickle
+import numpy as np
 
 
 log_model=pickle.load(open('https://github.com/subir8881/FIRST-DEPLOYMENT/blob/f2811167a7eadb9ecc680c3de564134d803f1c02/log_model.pkl','rb'))
