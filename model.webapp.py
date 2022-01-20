cd6d2d0202ad270d8c92e9cda241f19337cfbb31
subir8881 <72200125+subir8881@users.noreply.github.com>
2022-01-20 13:58:20 +0530

    Update model.webapp.py

--- model.webapp.py
+++ model.webapp.py
@@ -25,12 +25,12 @@ def main(hl=None):
     st.spinner("Hello")
     hl = st.selectbox('Resort', 'City Hotel')
     lt = st.slider('Lead Time', )
-    WN = st.slider('stays in weekend nights', )
+    wm = st.slider('stays in weekend nights', )
     wn = st.slider('stays in week nights', )
     ad = st.slider('Select Adults', )
     ch = st.slider('Select Children', )
     ba = st.slider('Select Babies', )
-    inputs = [[hl, lt, WN, wn, ad, ch, ba]]
+    inputs = [[hl, lt, wm, wn, ad, ch, ba]]
     if st.button('Classify'):
         if option == 'Logistic Regression':
             st.success(classify(log_reg.predict(inputs)))
